e3756429cbc230a9e2b8f3d77ef1659c057f3430
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-07-20 22:47:33 -0300

    Contador de rounds

--- datapath.vhd
+++ datapath.vhd
@@ -313,9 +313,13 @@ mx_load_step_right: mux2_1x8 port map(E4, "11111111", mx_penalty_right_value, lo
 
 right_time_counter: ContadorTempo port map(R1, enable_right_time_counter, CLK, load_step_right, T_right_out, end_time_right);
 
--- Saídas dos contadores
+-- Saídas dos contadores de tempo
 
 conversor_left: DecBCD port map(T_left_out, T_left_BCD);
 conversor_right: DecBCD port map(T_right_out, T_right_BCD);
+
+-- Contador de rounds
+
+meu_contador_round: ContadorRound port map(R1, E4, CLK, X, end_round);
 
 end arc_data;
