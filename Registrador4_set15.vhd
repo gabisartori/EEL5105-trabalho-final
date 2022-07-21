88e388582387f7ad9c5d2976125fdbd97a12ffab
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-07-20 22:47:24 -0300

    Corrigindo registradores
    Nomes das saídas e entradas ajustadas para combinar com o resto do circuito

--- Registrador4_set15.vhd
+++ Registrador4_set15.vhd
@@ -2,8 +2,8 @@ library IEEE;
 use IEEE.Std_Logic_1164.all;
 entity Registrador4_set15 is port (
     CLK, RST, ENABLE: in std_logic;
-    valor_proximo: in std_logic_vector(3 downto 0);
-    valor: out std_logic_vector(3 downto 0)
+    D: in std_logic_vector(3 downto 0);
+    Q: out std_logic_vector(3 downto 0)
     );
 end Registrador4_set15;
 
@@ -13,9 +13,9 @@ begin
   process(CLK,RST)
     begin
         if (RST = '1') then
-            valor <= "1111";
+            Q <= "1111";
         elsif (CLK'event and CLK = '1' and ENABLE = '1') then
-            valor <= valor_proximo;
+            Q <= D;
         end if;
     end process;
  
