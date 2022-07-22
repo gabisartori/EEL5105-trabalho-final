1e9983386da0856a4b0df0457268baff10cc34d2
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-07-22 17:40:36 -0300

    corrigindo nomes de sinais enter-end

--- usertop.vhd
+++ usertop.vhd
@@ -12,7 +12,7 @@ port(
 end usertop;
 
 architecture rtl of usertop is
-    Signal R1, E1, E2, E3, E4, E5, E6, end_game, end_sequence, end_round, enter_left, enter_right, enter, reset: std_logic;
+    Signal R1, E1, E2, E3, E4, E5, E6, end_game, end_sequence, end_round, end_left, end_right, enter_left, enter_right, enter, reset: std_logic;
 
     component datapath is port(
         SW: in std_logic_vector(17 downto 0);
@@ -49,7 +49,7 @@ begin
     myButtonPlay: ButtonPlay port map(KEY(3), KEY(2), reset, CLK, enter_left, enter_right);
     myButtonSync: ButtonSync port map(KEY(1), KEY(0), CLK, enter, reset);
 
-    myDatapath: datapath port map(SW, CLK, enter_right, enter_left, R1, E1, E2, E3, E4, E5, E6, end_game, end_sequence, end_round, enter_left, enter_right, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, LEDR);
+    myDatapath: datapath port map(SW, CLK, enter_right, enter_left, R1, E1, E2, E3, E4, E5, E6, end_game, end_sequence, end_round, end_left, end_right, HEX7, HEX6, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, LEDR);
     myControlUnit: controlunit port map(enter, reset, CLK, end_game, end_sequence, end_round, enter_left, enter_right, R1, E1, E2, E3, E4, E5, E6);
 
 end rtl;
