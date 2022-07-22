0872c97bb8ee91a964fd3c49f6a04b8aa6eb6c06
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2022-07-22 13:58:59 -0300

    Modelo da unidade de controle

--- controlunit.vhd
+++ controlunit.vhd
@@ -8,8 +8,32 @@ port(
 );
 end controlunit;
 
-architecture rtl of controlunit is
-
+architecture rtl of usertop is
+    type ESTADOS is ();
+    signal ESTADO, PROXIMO_ESTADO: ESTADOS;
 begin
-
+    process(CLOCK, RESET)
+    begin
+        if (reset = '1') then
+            ESTADO <= INIT;
+        elsif (CLOCK'event and CLOCK = '1') then
+            ESTADO <= PROXIMO_ESTADO;
+        end if;
+    end process;
+    
+    process(CLOCK, ESTADO)
+    begin
+        case ESTADO is
+            when INIT =>
+                S <= "11";
+                if (B = '1') then
+                    PROXIMO_ESTADO <= ON1;
+                else
+                    PROXIMO_ESTADO <= INIT;
+                end if;
+            when ON1 =>
+                S <= "01";
+                PROXIMO_ESTADO <= OFF1;           
+        end case;
+    end process;
 end rtl;
